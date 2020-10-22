19741d0abee21dafa77c2996d6392acae8069899
tiance001 <1340618494@qq.com>
2020-10-22 09:00:42 +0000

    用户注册

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -30,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        JsonResource::withoutWrapping();
         //
     }
 }
