21902c85c098e9ca07bd97454926be6193da8117
tiance001 <1340618494@qq.com>
2020-11-03 07:29:33 +0000

    weapp update user

--- routes/api.php
+++ routes/api.php
@@ -82,6 +82,8 @@ Route::prefix('v1')->namespace('Api')
                 // 编辑登录用户信息
                 Route::patch('user', 'UsersController@update')
                         ->name('user.update');
+                Route::put('user', 'UsersController@update')
+                        ->name('user.update');
                 // 上传图片
                 Route::post('images', 'ImagesController@store')
                         ->name('images.store');
